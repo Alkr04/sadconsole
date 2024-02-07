c935cfec24bf946d25be92f8cdca9a467e026de9
Alkr04 <h23alkr@axevalla.fhsk.se>
2024-02-07 11:50:44 +0100

    part 4-more objects

---
+++ SadConsoleGame/Gameobjekt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadConsoleGame
+{
+    internal class Gameobjekt
+    {
+        private ColoredGlyph _mapApperance = new ColoredGlyph();
+        public Point Position { get; private set; }
+        public ColoredGlyph Apperance { get; set; }
+
+        public Gameobjekt(ColoredGlyph apperance, Point position, IScreenSurface hostingsurface)
+        {
+            Apperance = apperance;
+            Position = position;
+
+            hostingsurface.Surface[position].CopyAppearanceTo(_mapApperance);
+
+            DrawGameObjekt(hostingsurface);
+        }
+        private void DrawGameObjekt(IScreenSurface screenSurface)
+        {
+            Apperance.CopyAppearanceTo(screenSurface.Surface[Position]);
+            screenSurface.IsDirty = true;
+        }
+        public bool Move(Point newPosition, IScreenSurface screenSurface)
+        {
+            if (!screenSurface.Surface.IsValidCell(newPosition.X, newPosition.Y)) return false;
+            _mapApperance.CopyAppearanceTo(screenSurface.Surface[Position]);
+            screenSurface.Surface[newPosition].CopyAppearanceTo(_mapApperance);
+
+            Position = newPosition;
+            DrawGameObjekt(screenSurface);
+
+            return true;
+        }
+
+    }
+}
--- SadConsoleGame/RootScreen.cs
+++ SadConsoleGame/RootScreen.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SadConsole.Input;
 
 namespace SadConsoleGame;
 
 internal class RootScreen : ScreenObject
 {
-    private ScreenSurface _map;
+    private Map  _map;
+    private Gameobjekt _controlledobject;
     public RootScreen()
     {
-        _map = new ScreenSurface(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
-        _map.UseMouse = false;
+        _map = new Map(Game.Instance.ScreenCellsX, Game.Instance.ScreenCellsY - 5);
 
-        Children.Add(_map);
+        Children.Add(_map.SurfaceObject);
+    }
+    
+    public override bool ProcessKeyboard(Keyboard keyboard)
+    {
+        bool handeled = false;
+        if (keyboard.IsKeyPressed(Keys.W))
+        {
+            _controlledobject.Move(_controlledobject.Position + Direction.Up, _map.SurfaceObject);
+            handeled = false;
+        }
+        else if (keyboard.IsKeyPressed(Keys.S))
+        {
+            _controlledobject.Move(_controlledobject.Position + Direction.Down, _map.SurfaceObject);
+        }
+        if (keyboard.IsKeyPressed(Keys.A))
+        {
+            _controlledobject.Move(_controlledobject.Position + Direction.Left, _map.SurfaceObject);
+        }
+        else if (keyboard.IsKeyPressed(Keys.D))
+        {
+            _controlledobject.Move(_controlledobject.Position + Direction.Right, _map.SurfaceObject);
+        }
+        return handeled;
     }
 }
